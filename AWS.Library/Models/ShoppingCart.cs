6e5b5905c9ebde5500939a08c71765433bd0acf1
jholl1886 <jacobholleman1886@gmail.com>
2024-05-30 11:50:24 -0400

    new files

---
+++ AWS.Library/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using AWS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS.library.Models
+{
+    internal class ShoppingCart
+    {
+        int Id {  get; set; }
+
+        public List<Items>? Contents { get; set; }
+    }
+}
